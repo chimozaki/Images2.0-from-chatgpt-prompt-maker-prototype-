99c7394e84aca68c7ec424eeae7102dfd41e1dff
Chimo_Zaki <sdkn.93@gmail.com>
2026-04-29 07:40:16 +0900

    Add files via upload
    色々と改善を加えました。

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='images2-gemini-prompt-maker-v2';
+const CACHE_NAME='images2-chatgpt-prompt-maker-v60';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>key!==CACHE_NAME?caches.delete(key):null))));self.clients.claim()});
